1f9e8c877f2fe927066dffb1fa89581b27723b8e
HONGEUNTAEK <hongeuntaek09@gmail.com>
2025-05-16 01:48:16 +0900

    awards 80%

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 진행바
 (function () {
    'use strict'
 
@@ -11,31 +12,62 @@
 
    const onScroll = () => {
       const scrollTop = document.documentElement.scrollTop;
-      // console.log(scrollTop);
 
-      const height = //보여지는 영역을 제외한 길이
-         document.documentElement.scrollHeight - //스크롤의 총길이
-         document.documentElement.clientHeight //보여지는 영역 
 
-      //결국 이걸 통해 progress-bar 의 width 값을 채워줄 것임 
+      const height =
+         document.documentElement.scrollHeight -
+         document.documentElement.clientHeight
+
       const scrollWidth = (scrollTop / height) * 100;
-      $progressBar.style.width = scrollWidth + '%' //스타일 변경
+      $progressBar.style.width = scrollWidth + '%'
    }
 
    window.addEventListener('scroll', () => onScroll())
 
 })()
 
+// 헤더 백그라운드
 hetBody = document.querySelector('body');
 hetHeader = document.querySelector('header');
 
-hetBody.addEventListener('wheel',(event) => {
+hetBody.addEventListener('wheel', (event) => {
    let wheel = event.wheelDeltaY;
 
-   if(wheel > 0) {
+   if (wheel > 0) {
       hetHeader.setAttribute("style", "background-color: #transparent;")
    }
    else {
       hetHeader.setAttribute("style", "background: #12121263;")
-    }
+   }
 })
+
+const menuItems = document.querySelectorAll('.header_menu li a');
+
+menuItems.forEach(item => {
+   item.addEventListener('click', (e) => {
+      e.preventDefault();
+      
+      const targetId = item.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      window.scrollTo({
+         top: targetSection.offsetTop - document.querySelector('header').offsetHeight, 
+         behavior: 'smooth'
+      });
+
+      history.pushState(null, null, window.location.pathname);
+   });
+});
+
+const menuLogo = document.querySelector('.header_title');
+console.log(menuLogo)
+
+menuLogo.addEventListener('click', (e) => {
+   e.preventDefault();
+   window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'  // 부드러운 스크롤 동작
+   });
+   history.pushState(null, null, window.location.pathname);
+});
